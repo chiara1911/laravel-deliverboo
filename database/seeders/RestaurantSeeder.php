96a1b1045f21b9bcc7f016ea011ec760638cabf2
Finnedorn <daniele.giuntoli@outlook.com>
2024-02-08 19:17:39 +0100

    created User Restaurant Dish Type seeders, updated and corrected jsons files

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Restaurant;
+use Illuminate\Support\Str;
 
 class RestaurantSeeder extends Seeder
 {
@@ -13,5 +15,19 @@ class RestaurantSeeder extends Seeder
     public function run(): void
     {
         //
+        $json = file_get_contents(__DIR__ . '/data/restaurants.json');
+        $restaurants = json_decode($json, true);
+
+        foreach ($restaurants as $restaurant) {
+            $newRestaurant = new Restaurant();
+            $newRestaurant->name = $restaurant['name'];
+            $newRestaurant->address = $restaurant['address'];
+            $newRestaurant->vat = $restaurant['vat'];
+            $newRestaurant->user_id = $restaurant['user_id'];
+            $newRestaurant->slug = Str::slug($restaurant['name'], '-');
+            $newRestaurant->image = Str::slug($restaurant['name'], '-') . '.jpg';
+            $newRestaurant->save();
+            $newRestaurant->types()->sync($restaurant['types']);
+        }
     }
 }
--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Type;
 
 class TypeSeeder extends Seeder
 {
@@ -13,5 +14,13 @@ class TypeSeeder extends Seeder
     public function run(): void
     {
         //
+        $json = file_get_contents(__DIR__."/data/types.json");
+        $types = json_decode($json, true);
+
+        foreach($types as $type){
+            $newType = new Type();
+            $newType->name = $type['name'];
+            $newType->save();
+        }
     }
 }
