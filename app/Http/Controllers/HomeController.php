21d7509c410728997158c97cd7771a45ee02c65b
Riccardo Potalivo <riccardopotalivo@gmail.com>
2024-02-09 16:14:40 +0100

    home controller & navbar set

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+use App\Models\Restaurant;
+use App\Models\Dish;
+use App\Models\User;
+
 use Illuminate\Http\Request;
+
 
 class HomeController extends Controller
 {
     public function index()
     {
+        $currentUserId = Auth::id();
+        if (Auth::user()) {
+            $restaurant = Auth::user()->restaurant;
+            return view('home', compact('restaurant'));
+        }
+        // dd($restaurant);
+
         return view('home');
     }
 }
