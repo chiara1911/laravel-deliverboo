fb15ed256db087d183f850d5437fb8224631e4ca
ericaostini <ericaostini14@gmail.com>
2024-02-09 15:36:08 +0100

    edit page

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -3,6 +3,7 @@
     <section class="container mt-5">
         <h1>{{ $dish->name }}</h1>
         <p>{{ $dish->description }}</p>
+        <p>Ingredienti: {{ $dish->ingredients }}</p>
         <p>Prezzo: {{ $dish->price }}</p>
     </section>
 @endsection
