9683bd0cf7d9e3a2bd3250501e19be1380e3bccf
ericaostini <ericaostini14@gmail.com>
2024-02-12 12:27:40 +0100

    index blade order

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -15,6 +15,8 @@ class OrderController extends Controller
     public function index()
     {
         //
+        $orders = Order::all();
+        return view("admin.orders.index", compact("orders"));
     }
 
     /**
--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -9,81 +9,50 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-header">
-                        Tutti i piatti
+                        Tutti gli ordini
                     </div>
                     <table class="table">
                         <thead>
                             <tr>
-                                <th>Nome Piatto</th>
-                                <th class="text-center">Azioni</th>
+                                <th>Nome</th>
+                                <th>Cognome</th>
+                                <th>Indirizzo</th>
+                                <th>Email</th>
+                                <th>Telefono</th>
+                                <th>Prezzo totale</th>
+                                <th>Data e ora</th>
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($dishes as $dish)
+                            @foreach ($orders as $order)
                                 <tr>
                                     <td>
-                                        <p class="text-uppercase text-black ">
-                                            {{ $dish->name }}</p>
+                                        <p>
+                                            {{ $order->name }}</p>
                                     </td>
                                     <td>
-                                        <div class="d-flex align-items-center justify-content-evenly">
-                                            <a href="{{ route('admin.dishes.show', $dish->id) }}"
-                                                class="text-decoration-none btn btn-primary">
-                                                <i class="fa-solid fa-eye"></i>
-                                            </a>
-                                            <a href="{{ route('admin.dishes.edit', $dish->id) }}"
-                                                class="text-decoration-none btn btn-success">
-                                                <i class="fa-solid fa-pen-to-square"></i>
-                                            </a>
-                                            <form action="{{ route('admin.dishes.destroy', $dish->id) }}" method="POST">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="cancel-btn btn btn-danger ms-3"
-                                                    data-item-title="{{ $dish->name }}"><i
-                                                        class="fa-solid fa-trash-can"></i></button>
-                                            </form>
-                                        </div>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-
-
-                </div>
-                <div class="card">
-                    <div class="card-header">
-                        Tutti i piatti cancellati
-                    </div>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>Nome Piatto</th>
-                                <th class="text-center">Azioni</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($deletedDishes as $dish)
-                                <tr>
-                                    <td>
-                                        <p class="text-uppercase text-black ">
-                                            {{ $dish->name }}</p>
+                                        <p>
+                                            {{ $order->surname }}</p>
                                     </td>
                                     <td>
-                                        <div class="d-flex align-items-center justify-content-evenly">
-                                            <a href="{{ route('admin.dishes.show', $dish->id) }}"
-                                                class="text-decoration-none btn btn-primary">
-                                                <i class="fa-solid fa-eye"></i>
-                                            </a>
-
-                                            <form action="{{ route('admin.dishes.restore', $dish->id) }}" method="POST">
-                                                @csrf
-                                                {{-- @method('DELETE') --}}
-                                                <button type="submit" class="btn btn-danger ms-3"
-                                                    data-item-title="{{ $dish->name }}"><i
-                                                        class="fa-solid fa-rotate-left"></i></button>
-                                            </form>
-                                        </div>
+                                        <p>
+                                            {{ $order->address }}</p>
+                                    </td>
+                                    <td>
+                                        <p>
+                                            {{ $order->email }}</p>
+                                    </td>
+                                    <td>
+                                        <p>
+                                            {{ $order->phone }}</p>
+                                    </td>
+                                    <td>
+                                        <p>
+                                            {{ $order->total_price }}</p>
+                                    </td>
+                                    <td>
+                                        <p>
+                                            {{ date('d/m/Y - H:i', strtotime($order->created_at)) }}</p>
                                     </td>
                                 </tr>
                             @endforeach
@@ -95,23 +64,4 @@
             </div>
 
     </section>
-
-    <!-- Modal -->
-    <div class="modal fade" id="removeModal" tabindex="-1" aria-labelledby="removeModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="removeModalLabel">ATTENZIONE</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <p>Sei sicur* di voler eliminare <span id="modal-item-title"></span>?</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Si</button>
-                    <button type="button" class="btn btn-primary">No</button>
-                </div>
-            </div>
-        </div>
-    </div>
 @endsection
