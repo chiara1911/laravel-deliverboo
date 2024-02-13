920ee1743674ff7040ce76e21e2b25939cc2fbfe
Finnedorn <daniele.giuntoli@outlook.com>
2024-02-13 12:01:30 +0100

    fixed dropdown icons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -95,8 +95,8 @@
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 {{-- <a class="dropdown-item" href="{{ url('admin') }}">{{__('Dashboard')}}</a> --}}
                                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
+                                     document.getElementById('logout-form').submit();">
+                                    Logout
                                 </a>
 
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
