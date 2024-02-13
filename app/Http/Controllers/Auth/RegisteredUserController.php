a8c1f3d3371acd62d418b14195836300391098d4
giuliabaratella <giuliabaratella7@gmail.com>
2024-02-13 18:09:34 +0100

    fixed registration form

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -12,6 +12,11 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
+use App\Models\Type;
+use App\Models\Restaurant;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
+
 use Throwable;
 
 class RegisteredUserController extends Controller
@@ -21,7 +26,8 @@ class RegisteredUserController extends Controller
      */
     public function create(): View
     {
-        return view('auth.register');
+        $types = Type::all();
+        return view('auth.register', compact('types'));
     }
 
     /**
@@ -36,7 +42,12 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'surname' => ['required', 'string', 'max:255'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class]
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'restaurant_name' => ['required', 'max:255'],
+            'image' => ['image', 'max:4000', 'mimes:jpg'],
+            'vat' => ['required','min:11', 'max:11', 'unique:restaurants'],
+            'address' => ['required', 'max:255'],
+            'types'=> ['required', 'exists:types,id'],
         ]);
 
         // try {
@@ -56,10 +67,32 @@ class RegisteredUserController extends Controller
 
         $user = User::create([
             'name' => $request->name,
-            'surname' => $request->name,
+            'surname' => $request->surname,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+
+        $formDataRestaurant =[
+            'name' => $request['restaurant_name'],
+            'vat' => $request['vat'],
+            'address' => $request['address'],
+            'user_id' => $user->id,
+        ];
+        $slug = Str::slug($request['restaurant_name'], '-');
+        $formDataRestaurant['slug'] = $slug;
+
+
+        if ($request->hasFile('image')) {
+            $name = Str::slug($request['restaurant_name'], '-') . '.jpg';
+            $img_path = Storage::putFileAs('restaurants', $request['image'], $name);
+            $formDataRestaurant['image'] = $img_path;
+        } else {
+            $formDataRestaurant['image'] = 'restaurants/restaurant-placeholder.jpg';
+        }
+        $newRestaurant = Restaurant::create($formDataRestaurant);
+        $newRestaurant->types()->attach($request->types);
+
 
         event(new Registered($user));
 
