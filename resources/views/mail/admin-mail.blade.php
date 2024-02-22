7c8b793edaa8c162391f76a8411f145b8fd8118c
Finnedorn <daniele.giuntoli@outlook.com>
2024-02-22 23:01:55 +0100

    mail view updated

--- resources/views/mail/admin-mail.blade.php
+++ resources/views/mail/admin-mail.blade.php
@@ -4,24 +4,29 @@
     </h1>
     <p>
        nuovo ordine ricevuto, ecco i dati:
-
+       <ul>
+           @foreach ($adminMail->dishes as $dish)
+               <li>
+                   {{ $dish->name }} x {{ $dish->pivot->quantity }}
+               </li>
+           @endforeach
+       </ul>
     </p>
     <p>
-        {{ $adminMail->id }}
-    </p>
-    <p>
-        {{ $adminMail->name }}
-    </p>
-    <p>
-        {{ $adminMail->surname }}
-    </p>
-    <p>
-        {{ $adminMail->address }}
-    </p>
-    <p>
-        {{ $adminMail->email }}
-    </p>
-    <p>
-        {{ $adminMail->phone }}
+       Dati dell'utente
+       <ul>
+        <li>
+           nome: {{ $adminMail->name }} {{ $adminMail->surname }}
+        </li>
+        <li>
+           indirizzo: {{ $adminMail->address }}
+        </li>
+        <li>
+           email: {{ $adminMail->email }}
+        </li>
+        <li>
+           telefono: {{ $adminMail->phone }}
+        </li>
+       </ul>
     </p>
 </div>
