8f33cb5c2f512edd67ac462c4a49fb4e64460ec5
Finnedorn <daniele.giuntoli@outlook.com>
2024-02-22 22:52:33 +0100

    new mail feature

---
+++ resources/views/mail/admin-mail.blade.php
@@ -0,0 +1,27 @@
+<div>
+    <h1>
+        Nuovo Ordine!
+    </h1>
+    <p>
+       nuovo ordine ricevuto, ecco i dati:
+
+    </p>
+    <p>
+        {{ $adminMail->id }}
+    </p>
+    <p>
+        {{ $adminMail->name }}
+    </p>
+    <p>
+        {{ $adminMail->surname }}
+    </p>
+    <p>
+        {{ $adminMail->address }}
+    </p>
+    <p>
+        {{ $adminMail->email }}
+    </p>
+    <p>
+        {{ $adminMail->phone }}
+    </p>
+</div>
